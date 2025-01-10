c447433f85ee7487df95db8f5ae9da42deaf9947
Luis Arturo Torres Romero <54611814+L-Arturo-Torres-Romero@users.noreply.github.com>
2025-01-10 15:48:48 -0600

    update Levant differentiator
    update Levant differentiator

--- LevantDifferenciator/LevantDiff.m
+++ LevantDifferenciator/LevantDiff.m
@@ -4,39 +4,32 @@
 % Levant A. (1998). Robust exact differentiation via sliding mode technique. 
 % Automatica, 34(3), 379-384
 %*************************************************
-
 function LevantDiff()
-clear all;
-dt = 0.01;
-[t, X] = ode45(@LevantDiffOde,[0:dt:15],[0 0]);
-
-figure;
-%diffResult = diff(sin(t))/dt;
-plot(t,X(:,2),t,cos(t),t,sin(t)); title('Derivative comparation');
-%plot(t,X(:,2)); title('Derivative comparation');
-
+    clear all;
+    dt = 0.01;
+    [t, X] = ode45(@LevantDiffOde,[0:dt:15],[0 0]);
+    
+    figure;
+    plot(t,X(:,2),'--',t,cos(t),'-.',t,sin(t),'-'); title('Differential comparison');
+    legend('Levant Diff', 'Diff = cos(t)', 'Original = sin(t)', 'Location', 'best');
 end
 
-
-
-%*****************************************************
+%****************************************
 % Levant differentiator
-% 
-% 
+% ***************************************
 function dX = LevantDiffOde(t,X) 
-eta_u0=X(1);    eta_u1= X(2); 
-
- %Diff Gains
- du_ref=eta_u1;   
- lambda_u0=20;   lambda_u1=10;  
-
- u_ref = sin(t);
- 
-%Diferenciadores Levant
- d_eta_u0=-lambda_u0*sqrt(abs(eta_u0-u_ref))*sign(eta_u0-u_ref)+eta_u1;
- d_eta_u1=-lambda_u1*sign(eta_u1-d_eta_u0);
-      
-dX = [d_eta_u0;d_eta_u1];
+    z_0=X(1);    z_1= X(2); 
+    
+     %Diff Gains
+     du_ref=z_1;   
+     lambda_u0=20;   lambda_u1=10;  
+     u_ref = sin(t);
+     
+    %Levant Differentiator
+     d_z_0=-lambda_u0*sqrt(abs(z_0-u_ref))*sign(z_0-u_ref)+z_1;
+     d_z_1=-lambda_u1*sign(z_1-d_z_0);
+          
+    dX = [d_z_0;d_z_1];
 end
 
 
--- LevantDifferenciator/LevantDiffComparison.m
+++ LevantDifferenciator/LevantDiffComparison.m
@@ -15,30 +15,26 @@ diffFnc = diff(diffFnc)/dt;
 %plot(t,X(:,2),t,cos(t),t,sin(t)); title('Derivative comparation');
 figure
 plot(t,X(:,2),t(2:end),diffFnc); title('levant diff VS amtlab diff');
-
 end
-
-
 
 %*****************************************************
 % Levant differentiator
-% 
-% 
+% ***************************************************
 function dX = LevantDiffOde(t,X) 
-eta_u0=X(1);    eta_u1= X(2); 
+z_0=X(1);    z_1= X(2); 
 
  %Diff Gains
- du_ref=eta_u1;   
+ du_ref=z_1;   
  %lambda_u0=20;   lambda_u1=10;  % original parameters of the experiments
   lambda_u0=200;   lambda_u1=2000;   % moving this parameters the precision can be increased
 
  %u_ref = sin(t);
  u_ref = sin((2*pi*t^2)/10);
 %Diferenciadores Levant
- d_eta_u0=-lambda_u0*sqrt(abs(eta_u0-u_ref))*sign(eta_u0-u_ref)+eta_u1;
- d_eta_u1=-lambda_u1*sign(eta_u1-d_eta_u0);
+ d_z_0=-lambda_u0*sqrt(abs(z_0-u_ref))*sign(z_0-u_ref)+z_1;
+ d_z_1=-lambda_u1*sign(z_1-d_z_0);
       
-dX = [d_eta_u0;d_eta_u1];
+dX = [d_z_0;d_z_1];
 end
 
 
